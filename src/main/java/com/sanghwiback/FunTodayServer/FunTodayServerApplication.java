dcc8659fb892f33e61ae34042b3982d56459c816
백상휘 <sanghwiback@gmail.com>
2023-06-22 20:45:11 +0900

    Initial commit

---
+++ src/main/java/com/sanghwiback/FunTodayServer/FunTodayServerApplication.java
@@ -0,0 +1,13 @@
+package com.sanghwiback.FunTodayServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunTodayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunTodayServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanghwiback/FunTodayServer/FunTodayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanghwiback.FunTodayServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunTodayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
